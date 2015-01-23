eae6660067ed0419571178528f8075a65174e8ed
Sam Kumar <samkumar@berkeley.edu>
2015-01-23 11:16:31 -0800

    Added pool for saving message parts, recycled from load generator

--- sync2_quasar/sync2_quasar.go
+++ sync2_quasar/sync2_quasar.go
@@ -4,6 +4,11 @@ import (
     "fmt"
     "gopkg.in/mgo.v2"
     "github.com/SoftwareDefinedBuildings/sync2_quasar/parser"
+    "sync"
+    "time"
+    capnp "github.com/glycerine/go-capnproto"
+    cpint "github.com/SoftwareDefinedBuildings/quasar/cpinterface"
+	//uuid "code.google.com/p/go-uuid/uuid"
 )
 
 func main() {
@@ -20,4 +25,55 @@ func main() {
     fmt.Println(*parsed[0])
     
     session.Close()
+}
+
+// 120 points in each sync_output
+const POINTS_PER_MESSAGE int = 120
+
+type InsertMessagePart struct {
+	segment *capnp.Segment
+	request *cpint.Request
+	insert *cpint.CmdInsertValues
+	recordList *cpint.Record_List
+	pointerList *capnp.PointerList
+	record *cpint.Record
+}
+
+var insertPool sync.Pool = sync.Pool{
+	New: func () interface{} {
+		var seg *capnp.Segment = capnp.NewBuffer(nil)
+		var req cpint.Request = cpint.NewRootRequest(seg)
+		var insert cpint.CmdInsertValues = cpint.NewCmdInsertValues(seg)
+		insert.SetSync(false)
+		var recList cpint.Record_List = cpint.NewRecordList(seg, POINTS_PER_MESSAGE)
+		var pointList capnp.PointerList = capnp.PointerList(recList)
+		var record cpint.Record = cpint.NewRecord(seg)
+		return InsertMessagePart{
+			segment: seg,
+			request: &req,
+			insert: &insert,
+			recordList: &recList,
+			pointerList: &pointList,
+			record: &record,
+		}
+	},
+}
+
+func insert_stream(uuid []byte, data *parser.Sync_Output) {
+    // TODO insert the data into quasar
+    return
+}
+
+func process() {
+    // TODO check if there are any files in Mongo left to insert
+    return
+}
+
+func process_loop() {
+    for true {
+        fmt.Println("looping")
+        process()
+        fmt.Println("sleeping")
+        time.Sleep(time.Second)
+    }
 }
