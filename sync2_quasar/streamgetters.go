f317579b1e26c968a5c20e50575a79385d14a164
Sam Kumar <samkumar@berkeley.edu>
2015-01-23 23:33:14 -0800

    Created prototype that passes initial tests; not yet complete

--- sync2_quasar/streamgetters.go
+++ sync2_quasar/streamgetters.go
@@ -6,54 +6,56 @@ import "github.com/SoftwareDefinedBuildings/sync2_quasar/parser"
    but it's needed since the data is parsed to structs, which as far as I know
    can't be subscripted with strings. */
 
-func GetL1Mag(index int, obj *parser.Sync_Output) float32 {
-    return obj.Sync_Data.L1MagAng[index].Mag
+func GetL1Mag(index int, obj *parser.Sync_Output) float64 {
+    return float64(obj.Sync_Data.L1MagAng[index].Mag)
 }
 
-func GetL1Ang(index int, obj *parser.Sync_Output) float32 {
-    return obj.Sync_Data.L1MagAng[index].Angle
+func GetL1Ang(index int, obj *parser.Sync_Output) float64 {
+    return float64(obj.Sync_Data.L1MagAng[index].Angle)
 }
 
-func GetL2Mag(index int, obj *parser.Sync_Output) float32 {
-    return obj.Sync_Data.L2MagAng[index].Mag
+func GetL2Mag(index int, obj *parser.Sync_Output) float64 {
+    return float64(obj.Sync_Data.L2MagAng[index].Mag)
 }
 
-func GetL2Ang(index int, obj *parser.Sync_Output) float32 {
-    return obj.Sync_Data.L2MagAng[index].Angle
+func GetL2Ang(index int, obj *parser.Sync_Output) float64 {
+    return float64(obj.Sync_Data.L2MagAng[index].Angle)
 }
 
-func GetL3Mag(index int, obj *parser.Sync_Output) float32 {
-    return obj.Sync_Data.L3MagAng[index].Mag
+func GetL3Mag(index int, obj *parser.Sync_Output) float64 {
+    return float64(obj.Sync_Data.L3MagAng[index].Mag)
 }
 
-func GetL3Ang(index int, obj *parser.Sync_Output) float32 {
-    return obj.Sync_Data.L3MagAng[index].Angle
+func GetL3Ang(index int, obj *parser.Sync_Output) float64 {
+    return float64(obj.Sync_Data.L3MagAng[index].Angle)
 }
 
-func GetC1Mag(index int, obj *parser.Sync_Output) float32 {
-    return obj.Sync_Data.C1MagAng[index].Mag
+func GetC1Mag(index int, obj *parser.Sync_Output) float64 {
+    return float64(obj.Sync_Data.C1MagAng[index].Mag)
 }
 
-func GetC1Ang(index int, obj *parser.Sync_Output) float32 {
-    return obj.Sync_Data.C1MagAng[index].Angle
+func GetC1Ang(index int, obj *parser.Sync_Output) float64 {
+    return float64(obj.Sync_Data.C1MagAng[index].Angle)
 }
 
-func GetC2Mag(index int, obj *parser.Sync_Output) float32 {
-    return obj.Sync_Data.C2MagAng[index].Mag
+func GetC2Mag(index int, obj *parser.Sync_Output) float64 {
+    return float64(obj.Sync_Data.C2MagAng[index].Mag)
 }
 
-func GetC2Ang(index int, obj *parser.Sync_Output) float32 {
-    return obj.Sync_Data.C2MagAng[index].Angle
+func GetC2Ang(index int, obj *parser.Sync_Output) float64 {
+    return float64(obj.Sync_Data.C2MagAng[index].Angle)
 }
 
-func GetC3Mag(index int, obj *parser.Sync_Output) float32 {
-    return obj.Sync_Data.C3MagAng[index].Mag
+func GetC3Mag(index int, obj *parser.Sync_Output) float64 {
+    return float64(obj.Sync_Data.C3MagAng[index].Mag)
 }
 
-func GetC3Ang(index int, obj *parser.Sync_Output) float32 {
-    return obj.Sync_Data.C3MagAng[index].Angle
+func GetC3Ang(index int, obj *parser.Sync_Output) float64 {
+    return float64(obj.Sync_Data.C3MagAng[index].Angle)
 }
 
-func GetLockstate(index int, obj *parser.Sync_Output) int32 {
-    return obj.Sync_Data.Lockstate[index]
+func GetLockstate(index int, obj *parser.Sync_Output) float64 {
+    return float64(obj.Sync_Data.Lockstate[index])
 }
+
+var insertGetters [13]func(int, *parser.Sync_Output) float64 = [13]func(int, *parser.Sync_Output) float64{GetL1Mag, GetL1Ang, GetL2Mag, GetL2Ang, GetL3Mag, GetL3Ang, GetC1Mag, GetC1Ang, GetC2Mag, GetC2Ang, GetC3Mag, GetC3Ang, GetLockstate}
--- sync2_quasar/sync2_quasar.go
+++ sync2_quasar/sync2_quasar.go
@@ -5,35 +5,80 @@ import (
     "gopkg.in/mgo.v2"
     "github.com/SoftwareDefinedBuildings/sync2_quasar/parser"
     "net"
+    "os"
     "sync"
     "time"
     capnp "github.com/glycerine/go-capnproto"
     cpint "github.com/SoftwareDefinedBuildings/quasar/cpinterface"
-	//uuid "code.google.com/p/go-uuid/uuid"
+	uuid "code.google.com/p/go-uuid/uuid"
 )
 
 func main() {
+    args := os.Args[1:]
+    if len(args) != 14 {
+        fmt.Println("14 arguments are required.")
+        fmt.Println("The first argument is the serial number of the uPMU we are inserting data for.")
+        fmt.Println("The remaining 13 arguments are the UUIDs of the streams for the uPMUs.")
+        fmt.Println("They must be in the following order:")
+        fmt.Println("L1 Magnitude")
+        fmt.Println("L1 Angle")
+        fmt.Println("L2 Magnitude")
+        fmt.Println("L2 Angle")
+        fmt.Println("L3 Magnitude")
+        fmt.Println("L3 Angle")
+        fmt.Println("C1 Magnitude")
+        fmt.Println("C1 Angle")
+        fmt.Println("C2 Magnitude")
+        fmt.Println("C2 Angle")
+        fmt.Println("C3 Magnitude")
+        fmt.Println("C3 Angle")
+        fmt.Println("Lock State")
+        return
+    }
+    var serial_number string = args[0]
+    var uuids = make([][]byte, NUM_STREAMS)
+    var connections = make([]net.Conn, NUM_STREAMS)
+    var sendLocks = make([]*sync.Mutex, NUM_STREAMS)
+    var recvLocks = make([]*sync.Mutex, NUM_STREAMS)
+    
+    var i int
+    
+    for i = 0; i < NUM_STREAMS; i++ {
+        uuids[i] = uuid.Parse(args[i + 1])
+        sendLocks[i] = &sync.Mutex{}
+        recvLocks[i] = &sync.Mutex{}
+    }
+    
+    var err error
+    for i = 0; i < NUM_STREAMS; i++ {
+        connections[i], err = net.Dial("tcp", DB_ADDR)
+        if err != nil {
+            fmt.Printf("Error connecting to the database: %v\n", err)
+            return
+        }
+    }
+    
     session, err := mgo.Dial("localhost:27017")
     c := session.DB("upmu_database").C("received_files")
-    res := make(map[string]interface{})
-    err2 := c.Find(make(map[string]interface{})).One(&res)
-    fmt.Println(err)
-    fmt.Println(err2)
-    fmt.Printf("%T\n", res["data"])
+    //res := make(map[string]interface{})
+    //err2 := c.Find(make(map[string]interface{})).One(&res)
+    //fmt.Println(err)
+    //fmt.Println(err2)
+    //fmt.Printf("%T\n", res["data"])
     
-    var data []uint8 = res["data"].([]uint8)
-    var parsed []*parser.Sync_Output = parser.ParseSyncOutArray(data)
-    fmt.Println(*parsed[0])
+    //var data []uint8 = res["data"].([]uint8)
+    //var parsed []*parser.Sync_Output = parser.ParseSyncOutArray(data)
+    //fmt.Println(*parsed[0])
     
-    process(c, "hello")
+    process_loop(c, serial_number, uuids, connections, sendLocks, recvLocks)
     
     session.Close()
 }
 
 // 120 points in each sync_output
 const POINTS_PER_MESSAGE int = 120
-
 const DB_ADDR string = "localhost:4410"
+const NUM_STREAMS int = 13
 
 type InsertMessagePart struct {
 	segment *capnp.Segment
@@ -64,18 +109,9 @@ var insertPool sync.Pool = sync.Pool{
 	},
 }
 
-var connectionPool sync.Pool = sync.Pool{
-    New: func () interface{} {
-        conn, err := net.Dial("tcp", DB_ADDR)
-        if (err != nil) {
-            fmt.Printf("Error connecting to database: %v\n", err)
-        }
-        return conn
-    },
-}
+const ytagbase int = 1
 
-/** This function doesn't really use OUTPUT. It just is needed to GETVALUE can be called with the correct arguments. */
-func insert_stream(uuid []byte, output *parser.Sync_Output, getValue func (int, *parser.Sync_Output) float64, startTime int64) {
+func insert_stream(uuid []byte, output *parser.Sync_Output, getValue func (int, *parser.Sync_Output) float64, startTime int64, connection net.Conn, sendLock *sync.Mutex, recvLock *sync.Mutex, feedback chan int) {
     var mp InsertMessagePart = insertPool.Get().(InsertMessagePart)
     
     segment := mp.segment
@@ -98,23 +134,24 @@ func insert_stream(uuid []byte, output *parser.Sync_Output, getValue func (int, 
 	insert.SetValues(recordList)
 	request.SetInsertValues(insert)
     
-    var connection net.Conn = connectionPool.Get().(net.Conn)
-    
     var sendErr error
+    sendLock.Lock()
     _, sendErr = segment.WriteTo(connection)
+    sendLock.Unlock()
     
     insertPool.Put(mp)
     
     if sendErr != nil {
         fmt.Printf("Error in sending message: %v\n", sendErr)
-        connectionPool.Put(connection)
-        
+        feedback <- 1
         return
     }
+    feedback <- 0
     
+    recvLock.Lock()
     responseSegment, respErr := capnp.ReadFromStream(connection, nil)
+    recvLock.Unlock()
 	
-	connectionPool.Put(connection)	
 	if respErr != nil {
 		fmt.Printf("Error in receiving response: %v\n", respErr)
 		return
@@ -129,9 +166,74 @@ func insert_stream(uuid []byte, output *parser.Sync_Output, getValue func (int, 
     return
 }
 
-func process(coll *mgo.Collection, sernum string) {
-    var ytagbase int = 1
-    var query = map[string]interface{}{
+func process(coll *mgo.Collection, query map[string]interface{}, sernum string, uuids [][]byte, connections []net.Conn, sendLocks []*sync.Mutex, recvLocks []*sync.Mutex) {
+    var documents *mgo.Iter = coll.Find(query).Iter()
+    
+    var result map[string]interface{} = make(map[string]interface{})
+    
+    var continueIteration bool = documents.Next(&result)
+    
+    var parsed []*parser.Sync_Output
+    var synco *parser.Sync_Output
+    var timeArr [6]int32
+    var i int
+    var j int
+    var timestamp int64
+    var feedback chan int
+    var success bool
+    var err error
+    
+    for continueIteration {
+        success = true
+        feedback = make(chan int)
+        parsed = parser.ParseSyncOutArray(result["data"].([]uint8))
+        for i = 0; i < len(parsed); i++ {
+            synco = parsed[i]
+            timeArr = synco.Sync_Data.Times
+            fmt.Printf("time: %v\n", timeArr)
+            if timeArr[0] < 2010 || timeArr[0] > 2020 {
+                // if the year is outside of this range things must have gotten corrupted somehow
+                fmt.Printf("Rejecting bad date record: year is %v\n", timeArr[0])
+                continue
+            }
+            timestamp = time.Date(int(timeArr[0]), time.Month(timeArr[1]), int(timeArr[2]), int(timeArr[3]), int(timeArr[4]), int(timeArr[5]), 0, time.UTC).UnixNano()
+            fmt.Printf("timestamp: %v\n", timestamp)
+            for j = 0; j < NUM_STREAMS; j++ {
+                go insert_stream(uuids[j], synco, insertGetters[j], timestamp, connections[j], sendLocks[j], recvLocks[j], feedback)
+            }
+        }
+        for j = 0; j < NUM_STREAMS; j++ {
+            if <-feedback == 1 {
+                fmt.Println("Warning: data for a stream could not be sent")
+                success = false
+            }
+        }
+        if success {
+            err = coll.Update(map[string]interface{}{
+                "_id": result["_id"],
+            }, map[string]interface{}{
+                "$set": map[string]interface{}{
+                    "ytag": ytagbase,
+                },
+            })
+    
+            if err != nil {
+                fmt.Printf("Could not update ytag: %v\n", err)
+            }
+        }
+        continueIteration = documents.Next(&result)
+    }
+    
+    err = documents.Err()
+    if err != nil {
+        fmt.Printf("Could not iterate through documents: %v\n", err)
+    }
+    
+    return
+}
+
+func process_loop(coll *mgo.Collection, sernum string, uuids [][]byte, connections []net.Conn, sendLocks []*sync.Mutex, recvLocks []*sync.Mutex) {
+    query := map[string]interface{}{
         "serial_number": sernum,
         "xtag": map[string]bool{
             "$exists": false,
@@ -148,49 +250,11 @@ func process(coll *mgo.Collection, sernum string) {
             },
         },
     }
-    
-    var documents *mgo.Iter = coll.Find(query).Iter()
-    
-    var result map[string]interface{} = make(map[string]interface{})
-    
-    var continueIteration bool = documents.Next(&result)
-    
-    var parsed []*parser.Sync_Output
-    var synco *parser.Sync_Output
-    var timeArr [6]int32
-    var i int
-    var timestamp int64
-    
-    for continueIteration {
-        parsed = parser.ParseSyncOutArray(result["data"].([]uint8))
-        for i = 0; i < len(parsed); i++ {
-            synco = parsed[i]
-            timeArr = synco.Sync_Data.Times
-            fmt.Printf("time: %v\n", timeArr)
-            if timeArr[0] < 2010 || timeArr[0] > 2020 {
-                // if the year is outside of this range things must have gotten corrupted somehow
-                fmt.Printf("Rejecting bad date record: year is %v\n", timeArr[0])
-                continue
-            }
-            timestamp = time.Date(int(timeArr[0]), time.Month(timeArr[1]), int(timeArr[2]), int(timeArr[3]), int(timeArr[4]), int(timeArr[5]), 0, time.UTC).UnixNano()
-            fmt.Printf("timestamp: %v\n", timestamp)
-        }
-        continueIteration = documents.Next(&result)
-    }
-    
-    var err error = documents.Err()
-    if err != nil {
-        fmt.Printf("Could not iterate through documents: %v\n", err)
-    }
-    
-    return
-}
-
-func process_loop(coll *mgo.Collection, sernum string) {
     for true {
         fmt.Println("looping")
-        process(coll, sernum)
+        process(coll, query, sernum, uuids, connections, sendLocks, recvLocks)
         fmt.Println("sleeping")
         time.Sleep(time.Second)
+        break // for now, we break. We'll eventually remove this.
     }
 }
