f19e5222d4cc2f65207a7b2d1b19bd36d6b42b35
Sam Kumar <samkumar@berkeley.edu>
2015-11-10 17:37:09 -0800

    Finished converting spaces to tabs

--- sync2_quasar/streamgetters.go
+++ sync2_quasar/streamgetters.go
@@ -7,55 +7,55 @@ import "github.com/SoftwareDefinedBuildings/sync2_quasar/parser"
    can't be subscripted with strings. */
 
 func GetL1Mag(index int, obj *parser.Sync_Output) float64 {
-    return float64(obj.Sync_Data.L1MagAng[index].Mag)
+	return float64(obj.Sync_Data.L1MagAng[index].Mag)
 }
 
 func GetL1Ang(index int, obj *parser.Sync_Output) float64 {
-    return float64(obj.Sync_Data.L1MagAng[index].Angle)
+	return float64(obj.Sync_Data.L1MagAng[index].Angle)
 }
 
 func GetL2Mag(index int, obj *parser.Sync_Output) float64 {
-    return float64(obj.Sync_Data.L2MagAng[index].Mag)
+	return float64(obj.Sync_Data.L2MagAng[index].Mag)
 }
 
 func GetL2Ang(index int, obj *parser.Sync_Output) float64 {
-    return float64(obj.Sync_Data.L2MagAng[index].Angle)
+	return float64(obj.Sync_Data.L2MagAng[index].Angle)
 }
 
 func GetL3Mag(index int, obj *parser.Sync_Output) float64 {
-    return float64(obj.Sync_Data.L3MagAng[index].Mag)
+	return float64(obj.Sync_Data.L3MagAng[index].Mag)
 }
 
 func GetL3Ang(index int, obj *parser.Sync_Output) float64 {
-    return float64(obj.Sync_Data.L3MagAng[index].Angle)
+	return float64(obj.Sync_Data.L3MagAng[index].Angle)
 }
 
 func GetC1Mag(index int, obj *parser.Sync_Output) float64 {
-    return float64(obj.Sync_Data.C1MagAng[index].Mag)
+	return float64(obj.Sync_Data.C1MagAng[index].Mag)
 }
 
 func GetC1Ang(index int, obj *parser.Sync_Output) float64 {
-    return float64(obj.Sync_Data.C1MagAng[index].Angle)
+	return float64(obj.Sync_Data.C1MagAng[index].Angle)
 }
 
 func GetC2Mag(index int, obj *parser.Sync_Output) float64 {
-    return float64(obj.Sync_Data.C2MagAng[index].Mag)
+	return float64(obj.Sync_Data.C2MagAng[index].Mag)
 }
 
 func GetC2Ang(index int, obj *parser.Sync_Output) float64 {
-    return float64(obj.Sync_Data.C2MagAng[index].Angle)
+	return float64(obj.Sync_Data.C2MagAng[index].Angle)
 }
 
 func GetC3Mag(index int, obj *parser.Sync_Output) float64 {
-    return float64(obj.Sync_Data.C3MagAng[index].Mag)
+	return float64(obj.Sync_Data.C3MagAng[index].Mag)
 }
 
 func GetC3Ang(index int, obj *parser.Sync_Output) float64 {
-    return float64(obj.Sync_Data.C3MagAng[index].Angle)
+	return float64(obj.Sync_Data.C3MagAng[index].Angle)
 }
 
 func GetLockState(index int, obj *parser.Sync_Output) float64 {
-    return float64(obj.Sync_Data.Lockstate[index])
+	return float64(obj.Sync_Data.Lockstate[index])
 }
 
 var insertGetters [13]func(int, *parser.Sync_Output) float64 = [13]func(int, *parser.Sync_Output) float64{GetL1Mag, GetL1Ang, GetL2Mag, GetL2Ang, GetL3Mag, GetL3Ang, GetC1Mag, GetC1Ang, GetC2Mag, GetC2Ang, GetC3Mag, GetC3Ang, GetLockState}
